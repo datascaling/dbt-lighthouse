fec1c3f9aecb53c705a596fdc32e554c56b060c1
alex.pre@gmail.com <alex.pre@gmail.com>
2022-06-03 18:34:25 +0000

    orders table

--- models/customers.sql
+++ models/customers.sql
@@ -33,11 +33,16 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        orders.order_date,
+        orders.qty,
+        orders.unit_price,
+        orders.qty*orders.unit_price as order_total
+        -- coalesce(customer_orders.number_of_orders, 0) as number_of_orders
 
     from customers
 
     left join customer_orders using (customer_id)
+    left join orders using (customer_id)
 
 )
 
